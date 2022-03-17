94be75d0152266e541dd395886f44c1881a33f70
SylvainS94 <sylvain.sempere@orange.fr>
2022-03-17 12:29:21 +0100

    Add posts to home + create show_article

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController
 {
@@ -12,8 +14,12 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="default_home", methods={"GET"})
      */
-    public function home(): Response
+    public function home(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('default/home.html.twig');
+        $articles = $entityManager->getRepository(Article::class)->findBy(['deletedAt' => null]);
+
+        return $this->render('default/home.html.twig', [
+            'articles' => $articles,
+        ]);
     }
 } 
